b8923ac7426c3778f335582137736a0b05ebe8b5
Chirag B S <149285787+Rag04@users.noreply.github.com>
2025-07-03 15:23:35 +0530

    Update mongo_analysis.py

--- mongo_analysis.py
+++ mongo_analysis.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-client=MongoClient("mongodb+srv://user1:user123@cluster0.rnd2980.mongodb.net/")
+client=MongoClient("")
 
 db=client.books_data
 document=db.mystery_3.aggregate([
